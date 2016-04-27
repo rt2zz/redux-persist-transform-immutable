22d4c6be489efe9c3cf8eb06763c99f7165f3e0c
Zack Story <ztstory@gmail.com>
2016-04-26 20:19:07 -0700

    update to new transform format

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
-## Redux Persist Immutable
-Add immutable support to redux-persist
+## Redux Persist Transform Immutable
+Add immutable support to redux-persist. **NOTE** this handles immutable state on a per-reducer basis, but does not provide support for top level immutable state.
 
 ### Usage
 ```js
 import { compose } from 'redux'
 import { persistStore, autoRehydrate } from 'redux-persist'
-import reduxPersistImmutable from 'reduxPersistImmutable'
+import immutableTransform from 'redux-persist-transform-immutable'
 const reducer = combineReducers(reducers)
 const store = compose(autoRehydrate(), createStore)(reducer)
-persistStore(store, {transforms: [reduxPersistImmutable]})
+
+persistStore(store, {transforms: [immutableTransform()]})
 ```
--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 var transit = require('transit-immutable-js')
+var reduxPersist = require('redux-persist')
 
-module.exports = {
-  in: function(state){
-    if(state && typeof state === 'object'){
-      return transit.toJSON(state)
-    }
-    return state
-  },
-  out: function(raw){
-    if(typeof raw === 'string'){
-      return transit.fromJSON(raw)
-    }
-    return raw
-  }
+module.exports = function (config) {
+  return reduxPersist.createTransform(
+    function(state){
+      if(state && typeof state === 'object'){
+        return transit.toJSON(state)
+      }
+      return state
+    },
+    function(raw){
+      if(typeof raw === 'string'){
+        return transit.fromJSON(raw)
+      }
+      return raw
+    },
+    config
+  )
 }
