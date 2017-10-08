26c63556f7c8a63d5fc5dad9400d96d691e13020
Nathaniel Tucker <nathaniel@robinhood.com>
2017-10-08 14:54:33 -0700

    Use removedev-serialize instead of transit

--- index.js
+++ index.js
@@ -1,21 +1,11 @@
-var transit = require('transit-immutable-js')
+var Immutable = require('immutable')
+var Serialize = require('remotedev-serialize')
 var reduxPersist = require('redux-persist')
 
 module.exports = function (config) {
   config = config || {}
 
-  var transitInstance = transit
-  if (config.records) {
-    transitInstance = transit.withRecords(config.records)
-  }
+  var serializer =  Serialize.immutable(Immutable, config.records)
 
-  return reduxPersist.createTransform(
-    function(state){
-      return transitInstance.toJSON(state)
-    },
-    function(raw){
-      return transitInstance.fromJSON(raw)
-    },
-    config
-  )
+  return reduxPersist.createTransform(serializer.stringify, serializer.parse, config)
 }
