cb5758308adc9a4f539dc88f3deeed6cf4ff9c64
Saul Martínez <saul.martinez05@gmail.com>
2020-08-18 21:27:13 -0500

    Fixed code style.

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,6 @@
 declare module 'redux-persist-transform-immutable' {
-    import {Record} from 'immutable'
-    import {Transform} from 'redux-persist/es/types'
+    import { Record } from 'immutable'
+    import { Transform } from 'redux-persist/es/types'
 
     interface Config {
         records: (typeof Record | string)[]
